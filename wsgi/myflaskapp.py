221b3a3896558489cdde59801f561b6997db3ab4
Ricky Cook <mail@thatpanda.com>
2014-02-27 23:46:52 +1100

    Add the env route for debugging

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,9 +1,25 @@
-from flask import Flask
+import flask
+
+from flask import Flask, Response
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/env')
+def env():
+    lines = [
+        "{} => {}".format(key, value)
+        for key, value
+        in flask.request.environ.items()
+    ]
+
+    return Response(
+        "\n".join(lines),
+        content_type="text/plain;charset=UTF-8"
+    )
 
 if __name__ == "__main__":
     app.run()
