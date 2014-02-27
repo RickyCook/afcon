8de505e7793ff3b7fa6e1e077a660de6e1e7b813
Ricky Cook <mail@thatpanda.com>
2014-02-27 23:21:18 +1100

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+import os
+
 from setuptools import setup
 
-setup(name='AFCon',
-      version='0.1',
-      description='',
-      author='Ricky Cook',
-      author_email='mail@thatpanda.com',
-      url='https://github.com/RickyCook/afcon',
-      install_requires=['Flask==0.10.1', 'MarkupSafe'],
-      )
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+setup(
+    name='AFCon',
+    version='0.1',
+    description='',
+    author='Ricky Cook',
+    author_email='mail@thatpanda.com',
+    url='https://github.com/RickyCook/afcon',
+    install_requires=open(
+        '%s/requirements.txt' % (
+            os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)
+        )
+    ).readlines(),
+ )
