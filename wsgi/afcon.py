896293c2fa651e5b1f07ec29c1720ce60a57fa78
Ricky Cook <mail@thatpanda.com>
2014-02-27 23:58:42 +1100

    Remove unsafe env vars

--- wsgi/afcon.py
+++ wsgi/afcon.py
@@ -1,8 +1,11 @@
 import flask
+import os
+import re
 
 from flask import Flask, Response
 
 app = Flask(__name__)
+gear_uuid = getattr(os.environ, 'OPENSHIFT_GEAR_UUID', None)
 
 @app.route("/")
 def hello():
@@ -10,10 +13,16 @@ def hello():
 
 @app.route('/env')
 def env():
+    exclude_re = r'OPENSHIFT|PASSWORD|^PG'
+
     lines = [
         "{} => {}".format(key, value)
         for key, value
         in flask.request.environ.items()
+        if not (
+            re.search(exclude_re, key) or
+            (gear_uuid and re.search(gear_uuid, value))
+        )
     ]
 
     return Response(
