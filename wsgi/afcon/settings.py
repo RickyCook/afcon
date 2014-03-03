3313568caa8ed90f818967ad38972889d5d9339f
Ricky Cook <mail@thatpanda.com>
2014-03-03 19:48:25 +1100

    Tabs to spaces

--- wsgi/afcon/settings.py
+++ wsgi/afcon/settings.py
@@ -4,14 +4,14 @@ GEAR_UUID = getattr(os.environ, 'OPENSHIFT_GEAR_UUID', None)
 IS_OPENSHIFT = GEAR_UUID is not None
 
 DB_CONNECTION = getattr(os.environ,
-	                    'OPENSHIFT_POSTGRESQL_DB_URL',
-	                    'sqlite:///afcon.db',
-	                    )
+                        'OPENSHIFT_POSTGRESQL_DB_URL',
+                        'sqlite:///afcon.db',
+                        )
 
 LOGGING_CONFIG = {
-	'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 }
 if IS_OPENSHIFT:
-	LOGGING_CONFIG['level'] = logging.INFO
+    LOGGING_CONFIG['level'] = logging.INFO
 else:
-	LOGGING_CONFIG['level'] = logging.DEBUG
+    LOGGING_CONFIG['level'] = logging.DEBUG
