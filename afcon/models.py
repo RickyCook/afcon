1413b517b55e51bf0f2b24714c7100a413fec5c3
Ricky Cook <mail@thatpanda.com>
2014-03-24 21:00:27 +1100

    Change password field to PasswordType

--- afcon/models.py
+++ afcon/models.py
@@ -1,9 +1,11 @@
+from sqlalchemy_utils.types.password import PasswordType
+
 from afcon import db
 
 
 class User(db.Model):
     email = db.Column(db.String(254), primary_key=True)
-    pw_hash = db.Column(db.LargeBinary())
+    password = db.Column(PasswordType(schemes=['pbkdf2_sha512']))
 
     nick_name = db.Column(db.String(100))
     real_name = db.Column(db.String(100))
