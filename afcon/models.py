5bf6b43fec76ec856cbbe3b5a4499f14a0c307eb
Ricky Cook <mail@thatpanda.com>
2014-03-25 17:44:29 +1100

    Update User model
     - Add id column and make PK (hacky migration change)
     - Make fields not nullable

--- afcon/models.py
+++ afcon/models.py
@@ -4,9 +4,11 @@ from afcon import db
 
 
 class User(db.Model):
-    email = db.Column(db.String(254), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+
+    email = db.Column(db.String(254), nullable=False)
     password = db.Column(PasswordType(schemes=['pbkdf2_sha512']))
 
-    nick_name = db.Column(db.String(100))
-    real_name = db.Column(db.String(100))
-    phone = db.Column(db.String(13))
+    nick_name = db.Column(db.String(100), nullable=False)
+    real_name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(13), nullable=False)
