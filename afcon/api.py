b8d58403dbab236e22145541f9ffafcbee76557a
Ricky Cook <mail@thatpanda.com>
2014-03-27 19:34:48 +1100

    Add user editing functionality to the API

--- afcon/api.py
+++ afcon/api.py
@@ -19,16 +19,60 @@ class Users(Resource):
     """
     User details resource, used for getting a specific user
     """
-    def _get_user(self, email):
+    @classmethod
+    def _get_user(cls, email):
+        """
+        Gets the user specified by email or raises a 404 and aborts if a user
+        is not found
+        """
         try:
             return User.query.filter_by(email=email).one()
         except NoResultFound:
             abort(404, message="User {} doesn't exist.".format(email))
 
+    @classmethod
+    def _populate_user(cls, user):
+        """
+        Fills and saves the user object, with validation. Will add any fields
+        that don't exist in the form submission from the model (patch).
+
+        Returns True on success, dict of field errors on failure
+        """
+        form = UserForm()
+
+        # Set defaults from model
+        for field in form.data.keys():
+            if field not in request.form:
+                getattr(form, field).data = getattr(user, field)
+
+        if form.validate_on_submit():
+            form.populate_obj(user)
+            db.session.add(user)
+            db.session.commit()
+
+            return True
+
+        return form.errors
+
     @marshal_with(USER_FIELDS)
     def get(self, email):
+        """
+        List the user
+        """
         return self._get_user(email)
 
+    @marshal_with(USER_FIELDS)
+    def post(self, email):
+        """
+        Edit an existing user
+        """
+        obj = self._get_user(email)
+        result = self._populate_user(obj)
+
+        if result is True:
+            return obj, 201  # HTTP created
+        else:
+            return result, 400  # HTTP bad request
 
 class UsersList(Resource):
     """
@@ -36,17 +80,16 @@ class UsersList(Resource):
     """
     @marshal_with(USER_FIELDS)
     def post(self):
-        form = UserForm()
-        
-        if form.validate_on_submit():
-            obj = User()
-            form.populate_obj(obj)
-            db.session.add(obj)
-            db.session.commit()
+        """
+        Create a new user
+        """
+        obj = User()
+        result = Users._populate_user(obj)
 
+        if result is True:
             return obj, 201  # HTTP created
-
-        return form.errors, 400  # HTTP bad request
+        else:
+            return form.errors, 400  # HTTP bad request
 
 
 api.add_resource(Users, '/users/<string:email>')
